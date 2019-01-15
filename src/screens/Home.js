867f96d85da107ca45cef8f23742001eb0ecaaba
Brian Bolnick <brianbolnick@gmail.com>
2019-01-14 22:50:38 -0700

    initial navigation setup

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,215 +1,49 @@
-import React from 'react';
-import {
-	StyleSheet,
-	View,
-	StatusBar,
-	ActivityIndicator,
-	ScrollView,
-	AsyncStorage
-} from 'react-native';
-import { LinearGradient } from 'expo';
-import uuid from 'uuid/v1';
+import React from "react";
+import { ScrollView, Text, Linking, View } from "react-native";
+import { Card, Button } from "react-native-elements";
 
-import { primaryGradientArray } from 'utils/Colors';
-import Header from 'components/Header';
-import SubTitle from 'components/SubTitle';
-import Input from 'components/Input';
-import List from 'components/List';
-import Button from 'components/Button';
+const images = [
+  {
+    key: 1,
+    name: "Nathan Anderson",
+    image: require("../images/1.jpg"),
+    url: "https://unsplash.com/photos/C9t94JC4_L8"
+  },
+  {
+    key: 2,
+    name: "Jamison McAndie",
+    image: require("../images/2.jpg"),
+    url: "https://unsplash.com/photos/waZEHLRP98s"
+  },
+  {
+    key: 3,
+    name: "Alberto Restifo",
+    image: require("../images/3.jpg"),
+    url: "https://unsplash.com/photos/cFplR9ZGnAk"
+  },
+  {
+    key: 4,
+    name: "John Towner",
+    image: require("../images/4.jpg"),
+    url: "https://unsplash.com/photos/89PFnHKg8HE"
+  }
+];
 
-const headerTitle = 'Todo';
-
-export default class Home extends React.Component {
-	state = {
-		inputValue: '',
-		loadingItems: false,
-		allItems: {},
-		isCompleted: false
-	};
-
-	componentDidMount = () => {
-		this.loadingItems();
-	};
-
-	newInputValue = value => {
-		this.setState({
-			inputValue: value
-		});
-	};
-
-	loadingItems = async () => {
-		try {
-			const allItems = await AsyncStorage.getItem('Todos');
-			this.setState({
-				loadingItems: true,
-				allItems: JSON.parse(allItems) || {}
-			});
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	onDoneAddItem = () => {
-		const { inputValue } = this.state;
-		if (inputValue !== '') {
-			this.setState(prevState => {
-				const id = uuid();
-				const newItemObject = {
-					[id]: {
-						id,
-						isCompleted: false,
-						text: inputValue,
-						createdAt: Date.now()
-					}
-				};
-				const newState = {
-					...prevState,
-					inputValue: '',
-					allItems: {
-						...prevState.allItems,
-						...newItemObject
-					}
-				};
-				this.saveItems(newState.allItems);
-				return { ...newState };
-			});
-		}
-	};
-
-	deleteItem = id => {
-		this.setState(prevState => {
-			const allItems = prevState.allItems;
-			delete allItems[id];
-			const newState = {
-				...prevState,
-				...allItems
-			};
-			this.saveItems(newState.allItems);
-			return { ...newState };
-		});
-	};
-
-	completeItem = id => {
-		this.setState(prevState => {
-			const newState = {
-				...prevState,
-				allItems: {
-					...prevState.allItems,
-					[id]: {
-						...prevState.allItems[id],
-						isCompleted: true
-					}
-				}
-			};
-			this.saveItems(newState.allItems);
-			return { ...newState };
-		});
-	};
-
-	incompleteItem = id => {
-		this.setState(prevState => {
-			const newState = {
-				...prevState,
-				allItems: {
-					...prevState.allItems,
-					[id]: {
-						...prevState.allItems[id],
-						isCompleted: false
-					}
-				}
-			};
-			this.saveItems(newState.allItems);
-			return { ...newState };
-		});
-	};
-
-	deleteAllItems = async () => {
-		try {
-			await AsyncStorage.removeItem('Todos');
-			this.setState({ allItems: {} });
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	saveItems = newItem => {
-		const saveItem = AsyncStorage.setItem('Todos', JSON.stringify(newItem));
-	};
-
-	render() {
-		const { inputValue, loadingItems, allItems } = this.state;
-
-		return (
-			<LinearGradient colors={primaryGradientArray} style={styles.container}>
-				<StatusBar barStyle="light-content" />
-				<View style={styles.centered}>
-					<Header title={headerTitle} />
-				</View>
-				<View style={styles.inputContainer}>
-					<SubTitle subtitle={"What's Next?"} />
-					<Input
-						inputValue={inputValue}
-						onChangeText={this.newInputValue}
-						onDoneAddItem={this.onDoneAddItem}
-					/>
-				</View>
-				<View style={styles.list}>
-					<View style={styles.column}>
-						<SubTitle subtitle={'Recent Notes'} />
-						<View style={styles.deleteAllButton}>
-							<Button deleteAllItems={this.deleteAllItems} />
-						</View>
-					</View>
-
-					{loadingItems ? (
-						<ScrollView contentContainerStyle={styles.scrollableList}>
-							{Object.values(allItems)
-								.reverse()
-								.map(item => (
-									<List
-										key={item.id}
-										{...item}
-										deleteItem={this.deleteItem}
-										completeItem={this.completeItem}
-										incompleteItem={this.incompleteItem}
-									/>
-								))}
-						</ScrollView>
-					) : (
-						<ActivityIndicator size="large" color="white" />
-					)}
-				</View>
-			</LinearGradient>
-		);
-	}
-}
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1
-	},
-	centered: {
-		alignItems: 'center'
-	},
-	inputContainer: {
-		marginTop: 40,
-		paddingLeft: 15
-	},
-	list: {
-		flex: 1,
-		marginTop: 70,
-		paddingLeft: 15,
-		marginBottom: 10
-	},
-	scrollableList: {
-		marginTop: 15
-	},
-	column: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between'
-	},
-	deleteAllButton: {
-		marginRight: 40
-	}
-});
+export default ({navigation}) => (
+  <View style={{ flex: 1 }}>
+    <ScrollView contentContainerStyle={{ paddingVertical: 20 }}>
+      {images.map(({ name, image, url, key }) => (
+        <Card title={`CARD ${key}`} image={image} key={key}>
+          <Text style={{ marginBottom: 10 }}>
+            Photo by {name}.
+          </Text>
+          <Button
+            backgroundColor="#03A9F4"
+            title="VIEW NOW"
+            onPress={() => Linking.openURL(url)}
+          />
+        </Card>
+      ))}
+    </ScrollView>
+  </View>
+);
--- src/utils/Colors.js
+++ src/utils/Colors.js
@@ -1,6 +1,3 @@
-const primaryStart = '#f18a69';
-const primaryEnd = '#d13e60';
-export const primaryGradientArray = [primaryStart, primaryEnd];
 export const lightWhite = '#fcefe9';
 export const inputPlaceholder = '#f1a895';
 export const lighterWhite = '#f4e4e2';
@@ -9,3 +6,63 @@ export const circleActive = '#90ee90';
 export const itemListText = '#555555';
 export const itemListTextStrike = '#c4c4cc';
 export const deleteIconColor = '#bc2e4c';
+
+//Yellows
+export const yellow = "#F9DC5C";
+
+//Blues
+//export const blue = '	#009FFD';
+export const blue = "#3FA7D6";
+export const darkBlue = "#3489B0";
+
+// Whites
+export const white = "#ffffff";
+export const whiteSmoke = "#F5F5F6";
+export const offWhite = "#ECEDEE";
+
+// Grays
+export const darkGray = "#848586";
+export const lightGray = "#E8E9EB";
+export const mutedGray = "rgb(0,0,0,.15)";
+export const offGray = "rgb(0,0,0,.30)";
+
+//Reds
+export const red = "#FE4969";
+export const darkRed = "#D03C56";
+
+// Blacks
+export const black = "#2F2D2E";
+
+//Orange
+export const orange = "#F9665E";
+
+//Greens
+export const green = "#59CD90";
+export const darkGreen = "#49A876";
+
+//Gradients
+const primaryStart = '#f0617d';
+const primaryEnd = red;
+export const primaryGradientArray = [primaryStart, primaryEnd];
+export const secondaryGradientArray = [blue, darkBlue];
+export const otherGradientArray = [green, darkGreen];
+
+export default {
+  whiteSmoke,
+  black,
+  blue,
+  darkBlue,
+  darkGray,
+  darkGreen,
+  darkRed,
+  green,
+  lightGray,
+  mutedGray,
+  offGray,
+  offWhite,
+  orange,
+	primaryGradientArray,
+  red,
+  white,
+  yellow
+};
